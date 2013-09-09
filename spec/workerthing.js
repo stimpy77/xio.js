fd035a662eb494775472c0f9da040100a005e0a9
Jon Davis <jon@jondavis.net>
2013-09-09 02:29:34 -0700

    web workers added

---
+++ spec/workerthing.js
@@ -0,0 +1,11 @@
+﻿debugger;
+var console = { log: function (msg) { postMessage({ "console.log": msg }); } };
+function start() {
+    console.log("starting starting");
+    setTimeout(function () {
+        postMessage({ result: "success", data: 42 });
+    }, 500);
+}
+self.onmessage = function (d) {
+    if (d.data == "start") start();
+}
---
+++ src/xio-iex.js
@@ -0,0 +1,27 @@
+﻿// Web Worker extension for Internet Explorer which does not support inline blob scripts
+var userpayload;
+self.onmessage = function (d) {
+    var m = d.data;
+    if (m === "start") {
+        start.apply(this, arguments);
+    }
+    if (m && m.userpayload) {
+        userpayload = eval("(" + m.userpayload + ")");
+    }
+};
+var __xiodebug = false;
+var emit = function() { postMessage.apply(this, arguments) };
+var console = { log: function (msg) { emit({ "console.log": msg }); } };
+var start = function () {
+    try {
+        if (__xiodebug) {
+            console.log("starting");
+        }
+        var result = userpayload.apply(this, arguments);
+        emit({ result: "success", data: result });
+        self.close();
+    } catch (e) {
+        emit({ result: "error", error: e });
+        throw e;
+    }
+};
